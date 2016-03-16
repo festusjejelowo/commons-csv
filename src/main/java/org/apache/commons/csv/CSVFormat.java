61456184648c61f6e1a641200cc66f47157d84fa
Jejelowo B. Festus <festus.jejelowo@outlook.com>
2016-03-16 16:50:30 +0100

    reduce the sleep time

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -797,7 +797,7 @@ public final class CSVFormat implements Serializable {
         for (int i=0; i<20; i++) {
             System.out.print('.');
             try {
-                Thread.currentThread().sleep(100);
+                Thread.currentThread().sleep(10);
             } catch (InterruptedException e) {
                 break;
             }
